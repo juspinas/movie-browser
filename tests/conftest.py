dc2df659c3c723703bfd774bd1fcf24860e55a86
juspinas <38094067+juspinas@users.noreply.github.com>
2020-10-23 12:53:54 +1300

    Ability to click director and actor names on movie page

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,3 +23,21 @@ def client():
     })
 
     return my_app.test_client()
+
+class AuthenticationManager:
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='thorke', password='cLQ^C#oFXloS'):
+        return self._client.post(
+            'authentication/login',
+            data={'username': username, 'password': password}
+        )
+
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+
+@pytest.fixture
+def auth(client):
+    return AuthenticationManager(client)
